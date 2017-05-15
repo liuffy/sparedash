aae532a4cd3bc358b59d1976c2ced4db38462cbd
Sophia Liu <sliu3@wellesley.edu>
2017-05-15 13:34:19 -0700

    Frontend file setup, achieve rendering

---
+++ frontend/components/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {  HashRouter as Router,
+				  Route,
+				  NavLink } from 'react-router-dom';
+import App from './app';
+import '../../css/app';
+import Home from './home';
+import Footer from './footer';
+import Portfolio from './portfolio';
+import Contact from './contact';
+import Faqs from './faqs';
+import Reviews from './reviews';
---
+++ frontend/components/root.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {  HashRouter as Router,
+	// Change back to BrowserRouter for prettier URL
+				  Route,
+				  NavLink } from 'react-router-dom';
+
+
+const Root = () => {
+  return(
+	  	<div>
+		  <h1>Hi!</h1>
+		  </div>
+		  )
+}
+
+export default Root;
